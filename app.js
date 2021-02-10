4a5c325cb54fc375941f775c350c0bb5113fb65d
teerayooti <teerayoot5056@gmail.com>
2021-02-10 21:29:14 +0700

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+var map = L.map('map', {
+    attributionControl: false
+}).setView([15.013713, 102.624093], 14);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+}).addTo(map);
